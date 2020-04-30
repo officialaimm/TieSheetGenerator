182fb3588e0d19de54ea29e2623cbc6abfe9486e
Ayush Ojha <aimm.official@gmail.com>
2020-04-30 09:28:10 +0545

    protected variables and functions. Cleanup with autopep8. Update requiremts.txt

--- tieSheet.py
+++ tieSheet.py
@@ -1,137 +1,148 @@
+# dependency modules
+import xlsxwriter
+
+# standard modules
 from itertools import combinations
 from random import shuffle
 from math import ceil
-import xlsxwriter
+
 
 class TieSheet:
-    def __init__(self,playersEachGame,winnerScore,preferredGameEachDay,scoreRule={}):
-        self.playersEachGame = playersEachGame
-        self.winnerScore = winnerScore
-        self.preferredGameEachDay = preferredGameEachDay
-        self.scoreRule = scoreRule
-        #CONSTANTS
+
+    def __init__(self, playersEachGame):
+        self._playersEachGame = playersEachGame
+        # CONSTANTS
         #
-        #FIXTURE_CONSTANT
-        self.FIXTURES_TITLE = "Fixtures"
-        self.FIXTURES_INDEX = "Fixture"
-        self.FIXTURES_PLAYERS = "Participants"
-        self.FIXTURES_OUTSIDERS = "Outsiders"
-        self.FIXTURES_POINTS = "Points"
-        self.FIXTURES_DAY = "Day"
-        #TABLE_CONSTANT
-        self.TABLE_TITLE = "Table"
-        self.TABLE_INDEX = "S.N."
-        self.TABLE_PLAYER = "Participant"
-        self.TABLE_PLAYED = "Games Played"
-        self.TABLE_POINTS = "Points"
-        self.TABLE_WINS = "Wins"
-        #RULE_CONSTANT
-        self.RULES_TITLE = "Rules"
-    def generateFixture(self,participants,playersEachGame):
-        fixtures = list(combinations(participants,playersEachGame))
+        # FIXTURE_CONSTANT
+        self._FIXTURES_TITLE = "Fixtures"
+        self._FIXTURES_INDEX = "Fixture"
+        self._FIXTURES_PLAYERS = "Participants"
+        self._FIXTURES_OUTSIDERS = "Outsiders"
+        self._FIXTURES_POINTS = "Points"
+        self._FIXTURES_DAY = "Day"
+        # TABLE_CONSTANT
+        self._TABLE_TITLE = "Table"
+        self._TABLE_INDEX = "S.N."
+        self._TABLE_PLAYER = "Participant"
+        self._TABLE_PLAYED = "Games Played"
+        self._TABLE_POINTS = "Points"
+        self._TABLE_WINS = "Wins"
+        # RULE_CONSTANT
+        self._RULES_TITLE = "Rules"
+
+    def _generateFixture(self):
+        fixtures = list(combinations(self._participants, self._playersEachGame))
         shuffle(fixtures)
         return fixtures
-    def generateFixtureSheet(self):
-        sheet = self.xlsxFile.add_worksheet(self.FIXTURES_TITLE)
-        fixtures = self.generateFixture(self.participants,self.playersEachGame)
+
+    def _generateFixtureSheet(self):
+        sheet = self._xlsxFile.add_worksheet(self._FIXTURES_TITLE)
+        fixtures = self._generateFixture()
         row = 0
-        sheet.write_row(row,0,[
-            self.FIXTURES_INDEX,
-            self.FIXTURES_PLAYERS,
-            self.FIXTURES_OUTSIDERS,
-            self.FIXTURES_POINTS,
-            self.FIXTURES_DAY
+        sheet.write_row(row, 0, [
+            self._FIXTURES_INDEX,
+            self._FIXTURES_PLAYERS,
+            self._FIXTURES_OUTSIDERS,
+            self._FIXTURES_POINTS,
+            self._FIXTURES_DAY
         ])
-        #empty row
-        sheet.write_row(row,0,[])
-        row+=1
-        #will be handy for formula in table sheet
-        self.rowWithPointsStart = row+1
-        self.playerCol = "B"
-        self.pointsCol = "D"
-        for index,fixture in enumerate(fixtures):
-            #1-indexed
+        # empty row
+        sheet.write_row(row, 0, [])
+        row += 1
+        # will be handy for formula in table sheet
+        self._rowWithPointsStart = row+1
+        self._playerCol = "B"
+        self._pointsCol = "D"
+        for index, fixture in enumerate(fixtures):
+            # 1-indexed
             index = index+1
-            days = ceil(index/self.preferredGameEachDay)
-            #outsiders
-            outsiders = list(filter(lambda participant:participant not in fixture,self.participants))
-            #empty row
-            sheet.write_row(row,0,[])
-            row+=1
-            #indexed row
-            for playerIndex,player in enumerate(fixture):
-                sheet.write_row(row,0,[
-                    playerIndex==0 and index or '',
+            days = ceil(index/self._preferredGameEachDay)
+            # outsiders
+            outsiders = list(
+                filter(lambda participant: participant not in fixture, self._participants))
+            # empty row
+            sheet.write_row(row, 0, [])
+            row += 1
+            # indexed row
+            for playerIndex, player in enumerate(fixture):
+                sheet.write_row(row, 0, [
+                    playerIndex == 0 and index or '',
                     player,
-                    playerIndex<len(outsiders) and outsiders[playerIndex] or '',
+                    playerIndex < len(
+                        outsiders) and outsiders[playerIndex] or '',
                     '',
-                    playerIndex==0 and days or ''
+                    playerIndex == 0 and days or ''
                 ])
-                row+=1
-        #will be handy for formula in table sheet
-        self.rowWithPointsEnd = row
-    def generateTable(self):
-        sheet = self.xlsxFile.add_worksheet(self.TABLE_TITLE)
+                row += 1
+        # will be handy for formula in table sheet
+        self._rowWithPointsEnd = row
+
+    def _generateTable(self):
+        sheet = self._xlsxFile.add_worksheet(self._TABLE_TITLE)
         row = 0
-        sheet.write_row(row,0,[
-            self.TABLE_INDEX,
-            self.TABLE_PLAYER,
-            self.TABLE_PLAYED,
-            self.TABLE_POINTS,
-            self.TABLE_WINS
+        sheet.write_row(row, 0, [
+            self._TABLE_INDEX,
+            self._TABLE_PLAYER,
+            self._TABLE_PLAYED,
+            self._TABLE_POINTS,
+            self._TABLE_WINS
         ])
-        #empty row
-        sheet.write_row(row,0,[])
-        row+=1
-        for index,participant in enumerate(self.participants):
-            sheet.write_row(row,0,[
+        # empty row
+        sheet.write_row(row, 0, [])
+        row += 1
+        for index, participant in enumerate(self._participants):
+            sheet.write_row(row, 0, [
                 index+1,
                 participant,
-                #matches played
+                # matches played
                 "=COUNT(FILTER({fixtureSheet}!{fixturePointCol}{fixtureRowStart}:{fixturePointCol}{fixtureRowEnd},{fixtureSheet}!{fixturePlayerCol}{fixtureRowStart}:{fixturePlayerCol}{fixtureRowEnd}={column}{row}))"
                 .format(
-                    fixtureSheet=self.FIXTURES_TITLE,
-                    fixturePointCol=self.pointsCol,
-                    fixturePlayerCol=self.playerCol,
-                    fixtureRowStart=self.rowWithPointsStart,
-                    fixtureRowEnd=self.rowWithPointsEnd,
+                    fixtureSheet=self._FIXTURES_TITLE,
+                    fixturePointCol=self._pointsCol,
+                    fixturePlayerCol=self._playerCol,
+                    fixtureRowStart=self._rowWithPointsStart,
+                    fixtureRowEnd=self._rowWithPointsEnd,
                     column="B",
                     row=row+1
                 ),
-                #points
+                # points
                 "=SUM(FILTER({fixtureSheet}!{fixturePointCol}{fixtureRowStart}:{fixturePointCol}{fixtureRowEnd},{fixtureSheet}!{fixturePlayerCol}{fixtureRowStart}:{fixturePlayerCol}{fixtureRowEnd}={column}{row}))"
                 .format(
-                    fixtureSheet=self.FIXTURES_TITLE,
-                    fixturePointCol=self.pointsCol,
-                    fixturePlayerCol=self.playerCol,
-                    fixtureRowStart=self.rowWithPointsStart,
-                    fixtureRowEnd=self.rowWithPointsEnd,
+                    fixtureSheet=self._FIXTURES_TITLE,
+                    fixturePointCol=self._pointsCol,
+                    fixturePlayerCol=self._playerCol,
+                    fixtureRowStart=self._rowWithPointsStart,
+                    fixtureRowEnd=self._rowWithPointsEnd,
                     column="B",
                     row=row+1
                 ),
-                #wins
+                # wins
                 "=COUNT(FILTER({fixtureSheet}!{fixturePointCol}{fixtureRowStart}:{fixturePointCol}{fixtureRowEnd},({fixtureSheet}!{fixturePlayerCol}{fixtureRowStart}:{fixturePlayerCol}{fixtureRowEnd}={column}{row}) * ({fixtureSheet}!{fixturePointCol}{fixtureRowStart}:{fixturePointCol}{fixtureRowEnd}={score}) ))"
                 .format(
-                    fixtureSheet=self.FIXTURES_TITLE,
-                    fixturePointCol=self.pointsCol,
-                    fixturePlayerCol=self.playerCol,
-                    fixtureRowStart=self.rowWithPointsStart,
-                    fixtureRowEnd=self.rowWithPointsEnd,
+                    fixtureSheet=self._FIXTURES_TITLE,
+                    fixturePointCol=self._pointsCol,
+                    fixturePlayerCol=self._playerCol,
+                    fixtureRowStart=self._rowWithPointsStart,
+                    fixtureRowEnd=self._rowWithPointsEnd,
                     column="B",
                     row=row+1,
-                    score=self.winnerScore
+                    score=self._winnerScore
                 ),
             ])
-            row+=1
-    def generateRules(self):
-        sheet = self.xlsxFile.add_worksheet(self.RULES_TITLE)
-    def generate(self,participants,filename):
-        self.participants = participants
-        #create xlsx file with given filename
-        self.xlsxFile = xlsxwriter.Workbook(filename)
-        #start generating
-        self.generateFixtureSheet()
-        self.generateTable()
-        self.generateRules()
-        #close file
-        self.xlsxFile.close()
+            row += 1
+
+    def _generateRules(self):
+        sheet = self._xlsxFile.add_worksheet(self._RULES_TITLE)
+
+    def generate(self, winnerScore, preferredGameEachDay, participants, filename):
+        self._winnerScore = winnerScore
+        self._preferredGameEachDay = preferredGameEachDay
+        self._participants = participants
+        # create xlsx file with given filename
+        self._xlsxFile = xlsxwriter.Workbook(filename)
+        # start generating
+        self._generateFixtureSheet()
+        self._generateTable()
+        self._generateRules()
+        # close file
+        self._xlsxFile.close()
